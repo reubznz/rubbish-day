83124a2a6a7d6086fc25e5ceb6c5bfd4bea553b6
reubz <47412059+reubznz@users.noreply.github.com>
2023-04-07 22:19:13 +1200

    Update app.py
    Add support for running behind reverse proxy

--- api/app.py
+++ api/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, request
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+)
 
 @app.route("/", methods=['GET'])
 def rubbish_day():
