558e140ff107ab92049a7d208950f7b2896a5e31
reubz <47412059+reubznz@users.noreply.github.com>
2024-04-10 16:31:13 +1200

    Update app.py

--- api/app.py
+++ api/app.py
@@ -73,7 +73,7 @@ def rubbish_day():
     if addressId is None or len(addressId) != 11 :
         return json.dumps(output)
     httpheaders = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
     }
     baseUrl = 'https://www.aucklandcouncil.govt.nz'
     thisUrl = '/rubbish-recycling/rubbish-recycling-collections/Pages/collection-day-detail.aspx?an='+addressId
